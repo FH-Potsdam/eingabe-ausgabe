adfdeb9f15c1c81eeae584fd1012d09f58601a79
fabiantheblind <icke@fabianmoronzirfas.me>
2015-04-02 13:12:05 +0200

    lots of images

---
+++ js/main-1415.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+console.log("ready");
+$("#headline").fitText(1.2);
+$(".project-algo").shuffle();
+$(".project-crowd").shuffle();
+$(".project-bits").shuffle();
+$(".people").shuffle();
+
+});
