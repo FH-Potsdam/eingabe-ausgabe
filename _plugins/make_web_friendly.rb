a570c07d61d269644593c83729e3105a76c36500
fabiantheblind <icke@fabianmoronzirfas.me>
2016-04-04 11:10:00 +0200

    checking for missing media

--- _plugins/make_web_friendly.rb
+++ _plugins/make_web_friendly.rb
@@ -4,9 +4,9 @@ require 'liquid'
 # http://stackoverflow.com/questions/21446369/deleting-all-special-characters-from-a-string-ruby
 module WEBFriendly
   def make_web_friendly(url)
-    res = url.downcase
-    res = res.gsub(' ', '-')
-    res = res.gsub(/[^a-zA-Z0-9\-]/,"")
+    res = url.to_s.downcase
+    res = res.to_s.gsub(' ', '-')
+    res = res.to_s.gsub(/[^a-zA-Z0-9\-]/,"")
     return res
   end
 end
